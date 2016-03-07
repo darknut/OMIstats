84cda00d1cde72960b50ca6d801347786a8e4fc5
Felix Cuadrilla <golden.darknut@gmail.com>
2016-03-07 12:29:47 -0600

    Agregando la opcion para sortear tablas (en perfil de institucion por el momento)

---
+++ OMIstats/OMIstats/js/sortMedallas.js
@@ -0,0 +1,20 @@
+﻿function agregaSorter() {
+    $.tablesorter.addParser({
+        id: 'medalla',
+        is: function (s, table, cell, $cell) {
+            return false;
+        },
+        format: function (s, table, cell, cellIndex) {
+            var $cell = $(cell);
+            var data = $cell.attr('medalla');
+            if (data) {
+                if (data == 0)
+                    return 100;
+                return data;
+            }
+            return s;
+        },
+        parsed: false,
+        type: 'numeric'
+    });
+}
