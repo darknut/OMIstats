a1f99625e3e2a529186fe69bc81efbf360d4ee58
Felix Cuadrilla <felix.cuadrilla@gmail.com>
2018-02-26 18:33:55 -0800

    Agregando el OmegaUp puller

---
+++ OMIstats/OMIstats/Models/OmegaUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMIstats.Models
+{
+    /// <summary>
+    /// Clase para conectarse con OmegaUp
+    /// </summary>
+    public class OmegaUp
+    {
+    }
+}
---
+++ OmegaUpPuller/OmegaUpPuller/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using OMIstats.Utilities;
+
+namespace OmegaUpPuller
+{
+    class Program
+    {
+        private static void Setup()
+        {
+            Acceso.CADENA_CONEXION = ConfigurationManager.AppSettings["conexion"];
+        }
+
+        static void Main(string[] args)
+        {
+            Acceso db = new Acceso();
+        }
+    }
+}
