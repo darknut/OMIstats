d68f354ad63f3aa868ebe59cf2d379996f7fe9c4
Felix Cuadrilla <golden.darknut@gmail.com>
2018-03-29 18:44:43 -0700

    Agregando el js para scoreboard

---
+++ OMIstats/OMIstats/js/scoreboard.js
@@ -0,0 +1,45 @@
+﻿var lastServerUpdate = 0;
+var lastUpdateWithServer = 0;
+
+function setTimes(server, page) {
+    lastServerUpdate = server;
+    lastUpdateWithServer = page;
+    updateTimes();
+}
+
+function startTimer() {
+    setInterval(update, 1000);
+}
+
+function timeToText(element, seconds) {
+    var minutes = Math.round(seconds / 60);
+
+    var text = "";
+
+    if (minutes == 0)
+    {
+        text = seconds + " segundo";
+        if (seconds != 1)
+            text += "s";
+    }
+    else
+    {
+        text = minutes + " minuto";
+        if (minutes != 1)
+            text += "s";
+    }
+
+    element.innerHTML = text;
+}
+
+function updateTimes() {
+    lastServerUpdate++;
+    lastUpdateWithServer++
+
+    timeToText(document.getElementById("lastServerUpdate"), lastServerUpdate);
+    timeToText(document.getElementById("lastPageUpdate"), lastUpdateWithServer);
+}
+
+function update() {
+    updateTimes();
+}
