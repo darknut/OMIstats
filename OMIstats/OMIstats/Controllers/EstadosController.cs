4d48c490251a419e9b830681a6d84d1a923a4f14
Felix Cuadrilla <golden.darknut@gmail.com>
2018-04-18 01:11:03 -0700

    Mejoras a los lugares

--- OMIstats/OMIstats/Controllers/EstadosController.cs
+++ OMIstats/OMIstats/Controllers/EstadosController.cs
@@ -27,6 +27,14 @@ namespace OMIstats.Controllers
             ViewBag.olimpiadas = Olimpiada.obtenerOlimpiadas(TipoOlimpiada.OMI);
             ViewBag.estados = Medallero.obtenerTablaEstadosGeneral(TipoOlimpiada.OMI, out cabeceras);
             ViewBag.cabeceras = cabeceras;
+
+            int ultimoValido = 0;
+            for (int i = cabeceras.Length - 1; i >= 0; i--)
+                if (cabeceras[i])
+                    ultimoValido = i;
+
+            ViewBag.ultimoValido = ultimoValido;
+
             return View(Estado.obtenerEstados());
         }
     }
