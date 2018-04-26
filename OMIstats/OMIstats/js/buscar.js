21343b1d83a60b21470311819138766034a58986
Felix Cuadrilla <golden.darknut@gmail.com>
2018-04-26 15:08:30 -0700

    Agregando javascript para buscar

---
+++ OMIstats/OMIstats/js/buscar.js
@@ -0,0 +1,16 @@
+﻿var base;
+
+function preparar(url) {
+    base = url;
+    $(document).ready(function () {
+        $('#searchbox').keypress(function (e) {
+            if (e.keyCode == 13)
+                $('#buscarButton').click();
+        });
+    });
+}
+
+function buscar() {
+    var input = document.getElementById("searchbox");
+    window.location = base + "Buscar?query=" + input.value;
+}
