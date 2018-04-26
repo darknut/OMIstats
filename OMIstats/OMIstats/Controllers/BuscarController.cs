c4bb1c3724819c1682724ce188cf351ec2c77697
Felix Cuadrilla <golden.darknut@gmail.com>
2018-04-26 14:35:59 -0700

    Archivos vacíos para la busqued

---
+++ OMIstats/OMIstats/Controllers/BuscarController.cs
@@ -0,0 +1,21 @@
+﻿using OMIstats.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OMIstats.Controllers
+{
+    public class BuscarController : BaseController
+    {
+        //
+        // GET: /Buscar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OMIstats/OMIstats/Models/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OMIstats.Models
+{
+    public class SearchResult
+    {
+    }
+}
