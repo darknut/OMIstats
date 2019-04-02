4db3774186552daa4db186115ff23cd17b10b7d7
Felix Cuadrilla <felix.cuadrilla@gmail.com>
2019-04-01 23:44:32 -0700

    Agregando código para mostrar overlay

---
+++ OMIstats/OMIstats/js/overlay.js
@@ -0,0 +1,10 @@
+﻿function showOverlay(clave) {
+    setVisible('overlay-container', true);
+    setVisible('overlay', true);
+
+    var tr = document.getElementById(clave);
+    var tds = tr.getElementsByTagName("td");
+
+    var container = document.getElementById('overlay-temp');
+    container.textContent = tds[3].innerText;
+}
