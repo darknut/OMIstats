6eae353373e5b4e5ac8f6d305f54eb032eef4b88
Felix Cuadrilla <felix.cuadrilla@gmail.com>
2020-05-30 20:13:41 -0700

    Agregando controles P3

--- OMIstats/OMIstats/js/fechas.js
+++ OMIstats/OMIstats/js/fechas.js
@@ -16,6 +16,17 @@
     yearSuffix: ''
 };
 $.datepicker.setDefaults($.datepicker.regional['es']);
+$.datepicker._generateMonthYearHeader_original = $.datepicker._generateMonthYearHeader;
+$.datepicker._generateMonthYearHeader = function (inst, dm, dy, mnd, mxd, s, mn, mns) {
+    var header = $($.datepicker._generateMonthYearHeader_original(inst, dm, dy, mnd, mxd, s, mn, mns)),
+        years = header.find('.ui-datepicker-year');
+
+    // reverse the years
+    years.html(Array.prototype.reverse.apply(years.children()));
+
+    // return our new html
+    return $('<div />').append(header).html();
+}
 
 function setCampoFechas(ID, rango)
 {
