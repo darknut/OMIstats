0a0fa24f6e600b513c3497a71ad2599c58c459ff
Felix Cuadrilla <felix.cuadrilla@gmail.com>
2024-10-16 09:55:22 -0700

    BUGFIX: Al lider le ponen todos dos veces

--- OMIstats/OMIstats/Controllers/ProfileController.cs
+++ OMIstats/OMIstats/Controllers/ProfileController.cs
@@ -326,7 +326,7 @@ namespace OMIstats.Controllers
             }
 
             return File(Archivos.comprimeArchivos(
-                Archivos.Folder.DIPLOMAS, omi + "\\" + md.estado, todos ? null : clave, ".pdf"),
+                Archivos.Folder.DIPLOMAS, omi + "\\" + md.estado, todos ? null : clave, todos ? null : ".pdf"),
                 "application/zip", "Diplomas.zip");
         }
 
